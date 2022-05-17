acca4899be8a90b20c81f23ef7a527045d04da05
yanis <yanis@ipedis.com>
2022-05-17 10:22:58 +0400

    Add EventId test case

--- tests/Domain/Core/ValueObject/EventIdTest.php
+++ tests/Domain/Core/ValueObject/EventIdTest.php
@@ -1,1 +1,12 @@
 <?php
+
+use Domain\Core\Event\EventId;
+use Domain\Core\ValueObject\EntityId;
+
+beforeEach(function () {
+    $this->eventId = EventId::generate();
+});
+
+it('Should be instance of EntityId', function () {
+    expect($this->eventId)->toBeInstanceOf(EntityId::class);
+});
