41cadaa22c1ae0327251229c56f423469e867987
yanis <yanis@ipedis.com>
2022-05-18 11:57:29 +0400

    feat AggregateRoot - add test case

--- tests/Domain/Core/Aggregate/AggregateRootAbstractTest.php
+++ tests/Domain/Core/Aggregate/AggregateRootAbstractTest.php
@@ -1,1 +1,89 @@
 <?php
+
+use Domain\Core\Aggregate\AggregateRootAbstract;
+use Domain\Core\Contract\DomainEntityInterface;
+use Domain\Core\Contract\EntityIdInterface;
+use Domain\Core\Event\DomainEventAbstract;
+use Domain\Core\ValueObject\EntityId;
+
+beforeEach(function () {
+    $this->userId = UserId::fromString('6eadc797-c7a8-4c8c-b20d-71c8017c9163');
+    $this->user = new User($this->userId);
+});
+
+it('should implement Domain Entity Interface', fn() => expect($this->user)->toBeInstanceOf(DomainEntityInterface::class));
+it('should return not have any events by default', function() {
+ expect(iterator_count($this->user->getRecordedEvents()->getIterator()))
+     ->toBe(0)
+     ->and($this->user->hasEvents())
+     ->toBe(false)
+ ;
+});
+it ('should be able to record events', function() {
+    $user = User::create();
+    expect(iterator_count($user->getRecordedEvents()->getIterator()))
+        ->toBe(1)
+        ->and($user->hasEvents())
+        ->toBe(true)
+    ;
+});
+
+it('should be able to serialize', function() {
+    expect(json_encode($this->user))
+        ->json()
+        ->toHaveCount(2)
+        ->uuid->toBe('6eadc797-c7a8-4c8c-b20d-71c8017c9163')
+        ->foo->toBe('bar')
+    ;
+});
+
+it('should be able to clear the recorded events', function() {
+    $user = User::create();
+    $user->clearRecordedEvents();
+    expect(iterator_count($user->getRecordedEvents()->getIterator()))
+        ->toBe(0)
+        ->and($this->user->hasEvents())
+        ->toBe(false)
+    ;
+});
+
+class UserId extends EntityId { }
+
+class User extends AggregateRootAbstract {
+    public function __construct(EntityIdInterface $userId)
+    {
+        parent::__construct($userId);
+    }
+
+    public static function create(): User
+    {
+
+        $user = new User(UserId::generate());
+        $user->recordEvent(new UserWasCreatedEvent($user->getUuid()));
+        return $user;
+    }
+
+    public function jsonData(): array
+    {
+        return [
+            'foo' => 'bar',
+        ];
+    }
+}
+
+class UserWasCreatedEvent extends DomainEventAbstract {
+
+    public function __construct(private readonly EntityIdInterface $userId) {
+        parent::__construct('UserWasCreated');
+    }
+
+    public function getAggregateId(): EntityIdInterface
+    {
+        return $this->userId;
+    }
+
+    public function getAggregateClassName(): string
+    {
+        return User::class;
+    }
+}
